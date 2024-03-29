65e1b6c004aad2fac8bfded8fb0b3dbe116ad20e
Godzilla Queen <93568788+GodzillaQueen@users.noreply.github.com>
2024-03-29 14:24:05 +0700

    Initial commit

---
+++ application/views/layouts/_navbar.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CI SHOP</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="class nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="dropdown-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown-1">
+                <a href="/admin-category.html" class="dropdown-item">Kategori</a>
+                <a href="/admin-product.html" class="dropdown-item">Produk</a>
+                <a href="/admin-order.html" class="dropdown-item">Order</a>
+                <a href="/admin-users.html" class="dropdown-item">Pengguna</a>
+            </div>
+        </li>
+        </ul>
+      <ul class="navbar-nav right">
+        <li class="nav-item">
+            <a href="/cart.html" class="nav-link"><i class="fas fa-shopping-cart"></i> Cart(0) </a>
+        </li>
+        <li class="nav-item">
+            <a href="/login.html" class="nav-link"> Login </a>
+        </li>
+        <li class="nav-item">
+            <a href="/register.html" class="nav-link"> Register </a>
+        </li>
+        <li class="class nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="dropdown-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">admin</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown-2">
+                <a href="/profile.html" class="dropdown-item">Profil</a>
+                <a href="/orders.html" class="dropdown-item">Orders</a>
+                <a href="#" class="dropdown-item">Log Out</a>
+            </div>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
